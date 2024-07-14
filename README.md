e8745f5052acf9049617b6f28174ed8c4ee40954
KeoFoxy <arasdiel68@gmail.com>
2024-07-14 18:03:38 +0300

    Fonts, wallpaper, nvim submodule

---
+++ README.md
@@ -0,0 +1,7 @@
+## Install
+
+```sh
+git clone --recurse-submodules https://github.com/keofoxy/dotfiles.git
+cd .dotfiles
+git submodule update --init --recursive
+```
---
+++ install_brew_packages.sh
@@ -0,0 +1,25 @@
+#!bin/bash
+
+packages=(
+  "git"
+  "zsh"
+  "neovim"
+  "swiftformat"
+  "eza"
+  "neofetch"
+)
+
+install_package() {
+  if brew list "$1" &>/dev/null; then
+    echo "$1 already installed"
+  else
+    echo "Installing $1..."
+    brew install "$1"
+  fi
+}
+
+for package in "${packages[@]}"; do
+  install_package "$package"
+done
+
+echo "All packages installed"
